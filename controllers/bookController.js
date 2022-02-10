b7b12a6c2fb7ea70e4f6c12686a8771cb2bea406
n57r <n57r@fresnou.org>
2022-02-10 08:07:40 -0800

    Third

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -9,3 +9,52 @@ module.exports.viewProfile= async function (req,res){
     const book = await Book.findByPk(req.params.id);
     res.render('book/profile',{book});
 };
+module.exports.renderEditForm = async function(req,res){
+    const book = await Book.findByPk(req.params.id);
+    res.render('book/edit', {book, genres});
+};
+module.exports.updateBook = async function (req,res) {
+    const book = await Book.update({
+        title: req.body.title,
+        publisher: req.body.publisher,
+        genre: req.body.genre,
+        pages: req.body.pages,
+        image: req.body.image,
+        description: req.body.description
+    },{
+        where:{
+            id:req.params.id
+        }
+    });
+    res.redirect(`/books/profile/${req.params.id}`);
+};
+module.exports.renderAddForm= function (req, res) {
+    const book = {
+        title: '',
+        publisher:'',
+        genre: genres[0],
+        pages: '',
+        image:'',
+        description: ''
+    };
+    res.render('book/add',{book,genres});
+};
+module.exports.addBook = async function (req, res) {
+    const book = await Book.create({
+        title: req.body.title,
+        publisher: req.body.publisher,
+        genre: req.body.genre,
+        pages: req.body.pages,
+        image: req.body.image,
+        description: req.body.description
+    });
+    res.redirect(`/books/profile/${book.id}`);
+};
+module.exports.deleteBook = async function (req, res) {
+    await Book.destroy({
+        where: {
+            id:req.params.id
+        }
+    });
+    res.redirect('/books');
+};
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const bookController = require('../controllers/bookController.js');
+const authorController = require('../controllers/authorController.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -9,4 +10,13 @@ router.get('/', function(req, res, next) {
 
 router.get('/books', bookController.viewAll);
 router.get('/books/profile/:id', bookController.viewProfile);
+router.get('/books/edit/:id', bookController.renderEditForm);
+router.post('/books/edit/:id', bookController.updateBook);
+router.get('/books/add',bookController.renderAddForm);
+router.post('/books/add',bookController.addBook);
+router.get('/books/delete/:id', bookController.deleteBook);
+
+router.get('/authors', authorController.viewAll);
+router.get('/authors/profile/:id', authorController.viewProfile);
+router.get('/authors/edit', authorController.renderEditForm);
 module.exports = router;
